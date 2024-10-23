756fd7823df0c009e86d52ee6bf75d45e818fe8b
Joaquin Piccirillo <pichijoaco1@gmail.com>
2024-10-22 21:32:21 -0300

    areglo del app con errors

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from controllers.peliculas_controller import peliculas_bp
 from controllers.login_controller import login_bp
-from utils.manejador_errores import configurar_manejo_errores
+from erros.manejador_errores import configurar_manejo_errores
 
 app = Flask(__name__)
 
